af5afe0cb4173fc239dafc27377567ea50724b32
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-07 19:17:16 +0900

    Time: 5 ms (83.01%), Space: 77.5 MB (29.74%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int size = 0;
+        while (left < right) {
+            size = Math.max(size, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return size;
+    }
+}
